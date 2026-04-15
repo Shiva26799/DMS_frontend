81eae3e3aeed8ca76282d60fc539e95be5f894f9
Nityananda <nityananda@refactorq.com>
2026-04-15 12:28:09 +0530

    Issue fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -61,7 +61,10 @@ mongoose
         app.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
+
     })
     .catch((err) => {
         console.error("MongoDB connection error:", err);
     });
+
+export default app;
